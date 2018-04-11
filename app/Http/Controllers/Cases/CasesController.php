0044478f3e82e35649506a917c79a59668f52aff
zhangbo <zhangbo_3@126.com>
2018-04-11 22:40:28 +0800

    立案案件控制器

---
+++ app/Http/Controllers/Cases/CasesController.php
@@ -0,0 +1,33 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\Http\Controllers\CaseS;
+
+
+use App\Http\Controllers\Controller;
+use App\Service\Exceptions\ApiExceptions;
+use App\Service\Exceptions\Message;
+use Illuminate\Http\Request;
+use App\Service\CaseS\CaseService;
+
+class CasesController extends Controller
+{
+    public function __construct()
+    {
+
+    }
+    
+    public function getCaseList(Request $request, CaseService $caseService)
+    {
+        try {
+            return Message::success($caseService->getCaseListService($request, requestData($request)));
+        } catch (\Exception $exception) {
+            return ApiExceptions::handle($exception);
+        }
+    }
+}
