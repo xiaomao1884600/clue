63d60eb5bfc57933d6dff17d7e0494a4bab76816
zhangbo <zhangbo_3@126.com>
2018-06-20 21:48:41 +0800

    ziduan

--- app/Service/Cases/ProblemCluesService.php
+++ app/Service/Cases/ProblemCluesService.php
@@ -54,17 +54,20 @@ class ProblemCluesService extends BaseService
                 'npc_member' => $val['npc_member'], 'cppcc_member' => $val['cppcc_member'], 'servant' => $val['servant'],
                 'supervision' => $val['supervision'], 'department' => $val['department'], 'enterprise_nature' => $val['enterprise_nature'],
                 'enterprise_level' => $val['enterprise_level'], 'station' => $val['station'], 'enterprise_persion_level' => $val['enterprise_persion_level'],
-                'leader_violate' => $val['leader_violate']
+                'leader_violate' => $val['leader_violate'], 'party_member' => $val['party_member'], 'supervision_type_two' => $val['supervision_type_two'],
+                'unparty_supervision' => $val['unparty_supervision'], 'unsupervision_type_two' => $val['unsupervision_type_two'],
             ];
             $res['sheet'] = [//线索情况
                 'clue_number' => $val['clue_number'], 'user_number' => $val['user_number'], 'clue_accept_time' => $val['clue_accept_time'],
                 'clue_agency' => $val['clue_agency'], 'clue_verify' => $val['clue_verify'], 'clue_source' => $val['clue_source'],
                 'clue_violate' => $val['clue_violate'], 'clue_involve_law' => $val['clue_involve_law'], 'clue_disposal_type' => $val['clue_disposal_type'],
-                'clue_disposal_type_2' => $val['clue_disposal_type_2'], 'clue_measures' => $val['clue_measures'], 'clue_collection_money' => $val['clue_collection_money'],
+                'clue_disposal_type_2' => $val['clue_disposal_type_2'], 'clue_collection_money' => $val['clue_collection_money'],
+                'crime_of_duty' => $val['crime_of_duty'], 'other_offenses' => $val['other_offenses'],
                 'clue_redeem_money' => $val['clue_redeem_money']
             ];
             $res['sheet2'] = [//线索文书
-                'document_main_content' => $val['document_main_content'], 'document_disposal_report' => $val['document_disposal_report'], 'document_remark' => $val['document_remark']
+                'document_main_content' => $val['document_main_content'], 'document_disposal_report' => $val['document_disposal_report'], 'document_remark' => $val['document_remark'],
+                'clue_measures' => $val['clue_measures'], 'clue_measures_count_time' => $val['clue_measures_count_time'],
             ];
         }
         return $res;
